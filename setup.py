aa097e1d32f9b6d548a89549765a40e4b7533d93
Aziz <abdelaza@tcd.ie>
2023-02-12 13:43:18 +0000

    few changes to UI

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from apps import create_app
-from flask import render_template
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("login.html")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

d96caf6833e88d7444c0ea267cb5aaf68d5deb94
mhealy6 <mhealy6@tcd.ie>
2023-03-02 11:07:23 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,51 @@ app = Flask(__name__, static_folder="assets", static_url_path="")
 def index():
     return render_template("index.html")
 
-@app.route("/some-pie.html")
-def pie():
-    return render_template("some-pie.html")
 
+
+@app.route("/404")
+def error ():
+    return render_template("404.html")
+
+@app.route("/blank")
+def blank ():
+    return render_template("blank.html")
+
+@app.route("/button")
+def Button ():
+    return render_template("button.html")
+
+@app.route("/chart")
+def chart ():
+    return render_template("chart.html")
+
+@app.route("/element")
+def Element ():
+    return render_template("element.html")
+
+@app.route("/form")
+def form ():
+    return render_template("form.html")
+
+@app.route("/signin")
+def signin ():
+    return render_template("signin.html")
+
+@app.route("/signup")
+def signup ():
+    return render_template("signup.html")
+
+@app.route("/table")
+def table ():
+    return render_template("table.html")
+
+@app.route("/typography")
+def typography ():
+    return render_template("typography.html")
+
+@app.route("/widget")
+def widget ():
+    return render_template("widget.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)

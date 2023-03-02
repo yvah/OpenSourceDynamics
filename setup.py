181853b274ddb819efbeb29666bf1315d9adb073
Healyr7 <healyr7@tcd.ie>
2023-03-02 11:32:25 +0000

    removed unused files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="assets", static_url_path="")
+app = Flask(__name__, static_folder="templates", static_url_path="")
 
 @app.route("/")
 def index():

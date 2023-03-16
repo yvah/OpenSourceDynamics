32ffe466dffe633aa99a045c619a990a63d6af2a
taylorj8 <taylorj8@tcd.ie>
2023-03-16 22:27:30 +0000

    added comments

--- query.py
+++ query.py
@@ -5,10 +5,10 @@ import os
 from time import time
 
 comment_threshold = 10
-max_iterations = 1  # number of iterations that should run; -1 to keep going until all issues/prs fetched
+max_iterations = -1  # number of iterations that should run; -1 to keep going until all issues/prs fetched
 # first in each tuple is
-pull_rates = [(100, 2), (90, 7), (80, 9), (70, 12), (60, 15), (50, 20), (40, 25), (30, 35),
-              (20, 58), (18, 70), (16, 75), (14, 80), (12, 95), (10, 100)]
+pull_rates = [(100, 3), (90, 7), (80, 9), (70, 12), (60, 15), (50, 20), (40, 25), (30, 35),
+              (25, 50), (20, 60), (18, 68), (16, 75), (14, 80), (12, 95), (10, 100)]
 
 
 # for timing how long it takes a function to run
@@ -25,7 +25,7 @@ def time_execution(function):
 
 
 @time_execution
-def run_query(auth, owner, repo, pull_type):  # TODO appending comments
+def run_query(auth, owner, repo, pull_type):
     print(f"Gathering {pull_type}...")
 
     # final list to be returned
@@ -77,7 +77,7 @@ def run_query(auth, owner, repo, pull_type):  # TODO appending comments
                 cursor = trimmed_request["pageInfo"]["endCursor"]
 
             # checks if any of the issues/prs have fewer than threshold comments
-            # if so, remove them and stop fetching
+            # if so, remove them and don't fetch any more
             last_count = trimmed_request["edges"][-1]["node"]["comments"]["totalCount"]
             if last_count < comment_threshold:
                 above_threshold = False
@@ -87,21 +87,27 @@ def run_query(auth, owner, repo, pull_type):  # TODO appending comments
                     else:
                         break
             else:
+                # determine the pull rate for the next iteration
                 for j, rate in enumerate(pull_rates):
                     if last_count <= rate[1]:
                         pr_index = j
                         break
 
             for j, edge in enumerate(trimmed_request["edges"]):
+                # filter out comments made by bots
                 node = edge["node"]
                 node["comments"]["edges"] = filter_comments(node["comments"]["edges"])
+
+                # update the totalCount
                 count = len(node["comments"]["edges"])
                 node["comments"]["totalCount"] = count
 
+                # pull the rest of the comments if there are any
                 if node["comments"]["pageInfo"]["hasNextPage"]:
                     comments = get_other_comments(node["number"], repo, owner, pull_type[0:-1],
                                                   headers, node["comments"]["pageInfo"]["endCursor"])
 
+                    # add comments to exiting ones, update totalCount, if under threshold comments, remove
                     if count + len(comments) >= comment_threshold:
                         node["comments"]["edges"] += comments
                         node["comments"]["totalCount"] += len(comments)
@@ -110,12 +116,13 @@ def run_query(auth, owner, repo, pull_type):  # TODO appending comments
                         trimmed_request["edges"].pop(j)
 
             json_list += trimmed_request["edges"]  # add to final list
-            print(f'{len(json_list)} {pull_type} gathered')
+            print(f'{len(json_list)} {pull_type} gathered')  # print progress
 
         else:
             print(f"Status code: {str(request.status_code)} on iteration {i}, pr_index = {pr_index}. Retrying")
             i -= 1
 
+    # write to file
     json_string = json.dumps(json_list, indent=4)
     write_to_file(json_string, repo, pull_type)
     # pprint(json_string)
@@ -158,6 +165,7 @@ def get_other_comments(number, repo, owner, p_type, headers, cursor=None):  # to
 
             filtered_comments = filter_comments(comments["edges"])
 
+            # add to list
             if comment_list is None:
                 comment_list = filtered_comments
             else:
@@ -192,42 +200,6 @@ def write_to_file(json_string, repo, p_type):
     # writing data into repoName_pullType.json in cwd/fetched_data directory
     with open(filepath + "/" + f"{repo}_{p_type}.json", "w") as outfile:
         outfile.write(json_string)
-
-
-# returns query for individual comments
-def get_ind_query(repo, owner, number, p_type, cursor=None):
-    # for pagination
-    if cursor is not None:
-        start_point = f', after: "{cursor}"'
-    else:
-        start_point = ""
-
-    query = """
-    {
-        repository(name: "%s", owner: "%s") {
-            %s(number: %d) {
-                comments(first:100%s) {
-                    edges {
-                        node {
-                            author {
-                                login
-                                __typename
-                            }
-                            bodyText
-                            createdAt
-                        }
-                    }
-                    pageInfo {
-                        hasNextPage
-                        endCursor
-                    }
-                }
-            }
-        }  
-    }
-    """ % (repo, owner, p_type, number, start_point)
-
-    return query
 
 
 # returns query for issue or pull request comments
@@ -278,6 +250,42 @@ def get_comments_query(repo, owner, p_type, pull_rate, cursor=None):
             }
         }
         """ % (repo, owner, p_type, pull_rate[0], start_point, pull_rate[1])
+
+    return query
+
+
+# returns query for individual comments
+def get_ind_query(repo, owner, number, p_type, cursor=None):
+    # for pagination
+    if cursor is not None:
+        start_point = f', after: "{cursor}"'
+    else:
+        start_point = ""
+
+    query = """
+    {
+        repository(name: "%s", owner: "%s") {
+            %s(number: %d) {
+                comments(first:100%s) {
+                    edges {
+                        node {
+                            author {
+                                login
+                                __typename
+                            }
+                            bodyText
+                            createdAt
+                        }
+                    }
+                    pageInfo {
+                        hasNextPage
+                        endCursor
+                    }
+                }
+            }
+        }  
+    }
+    """ % (repo, owner, p_type, number, start_point)
 
     return query
 

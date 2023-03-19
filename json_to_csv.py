37641578298ba067f645707510dd44c9ece62231
yvah <55929630+yvah@users.noreply.github.com>
2023-03-19 14:00:44 +0000

    Delete json_to_csv.py
    No need for this file.

--- json_to_csv.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_json(r'Users/macbookpro/PycharmProjects/SwEng-group13/fetched_data/flutter_issues.json')
-df.to_csv(r'Users/macbookpro/PycharmProjects/SwEng-group13/fetched_data/flutter_issues.csv', index = None)

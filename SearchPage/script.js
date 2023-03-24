56059ef369533889af55904c25081b7df276ae81
jcb4321 <bryansj@tcd.ie>
2023-03-24 16:10:43 +0000

    add javascript

--- SearchPage/script.js
+++ SearchPage/script.js
@@ -1,1 +1,10 @@
-fetch().then(rex => resizeBy.json()).then(data => {})
+
+      function go()
+      {
+         var name = document.getElementById('search').value;
+          var result = _.find(cities.locations, {'title': name});
+          if (!result)
+              window.alert('Nothing found');
+          else
+              window.alert('Go to ' + result.url);
+      }

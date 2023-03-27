5f4bdfd6d28377f4734998af9aefec78ef54c25c
jcb4321 <bryansj@tcd.ie>
2023-03-27 14:36:54 +0100

    more changes

--- Setup.py
+++ Setup.py
@@ -1,66 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="templates", static_url_path="")
-
+app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+if __name__ == '__main__':
+   app.run()
 # use https://raw.githubusercontent.com/yvah/SwEng-group13/API/query.py as source for query
-
-@app.route("/404")
-def error ():
-    return render_template("404.html")
-
-@app.route("/blank")
-def blank ():
-    return render_template("blank.html")
-
-@app.route("/button")
-def Button ():
-    return render_template("button.html")
-
-@app.route("/chart")
-def chart ():
-    return render_template("chart.html")
-
-@app.route("/element")
-def Element ():
-    return render_template("element.html")
-
-@app.route("/form")
-def form ():
-    return render_template("form.html")
-
-@app.route("/form", methods=["POST", "GET"])
-def retrieve():
-    # print(request.get_data(as_text=True))
-    return render_template("chart.html")
-
-@app.route("/charts", methods=["POST"])
-def charts ():
-    print(request.form["owner"])
-    return render_template("chart.html")
-
-@app.route("/signin")
-def signin ():
-    return render_template("signin.html")
-
-@app.route("/signup")
-def signup ():
-    return render_template("signup.html")
-
-@app.route("/table")
-def table ():
-    return render_template("table.html")
-
-@app.route("/typography")
-def typography ():
-    return render_template("typography.html")
-
-@app.route("/widget")
-def widget ():
-    return render_template("widget.html")
-
-if __name__ == "__main__":
-    app.run(debug = True)
---
+++ static/script.js
@@ -0,0 +1,12 @@
+
+function go()
+{
+   let pToken = document.getElementById('password')
+   pToken = pToken.value
+   let repo = document.getElementById('name')
+   repo = repo.value
+   let pullType = document.getElementById('pulltype')
+   console.log(repo);
+   console.log(pToken);
+   console.log(pullType);
+}

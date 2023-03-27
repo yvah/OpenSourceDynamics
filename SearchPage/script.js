3d6cf68cf7886858e12d4e2ab4197f24a5dc835f
jcb4321 <bryansj@tcd.ie>
2023-03-27 13:19:53 +0100

    fixing page

--- SearchPage/script.js
+++ SearchPage/script.js
@@ -1,15 +1,9 @@
 
       function go()
       {
-         let data = JSON.parse(jsonData)
          let pToken = document.getElementById('password').value
          let repo = document.getElementById('name').value
-         let x = document.querySelector('#list-holder');
-         x.innerHTML = ""
-        
-         for(i = 0; i<data.length;i++) {
-            let temp = data[i];
-        
-        }
-
+         let pullType = document.getElementById('pulltype').value
+         print(pToken)
+         print(repo)
       }

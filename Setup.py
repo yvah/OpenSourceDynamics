4b945a5086ad868149e10403971bbf2896e620d6
jcb4321 <bryansj@tcd.ie>
2023-03-28 16:30:19 +0100

    updating

--- Setup.py
+++ Setup.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="templates", static_url_path="")
+
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/query", methods=["POST"])
+def query():
+    # input tag in html requires name attribute that will be used in request.form[]
+    user = request.form["user"]
+    repo = request.form["repo"]
+    type = request.form["type"]
+    print(user, repo, type)
     return render_template("index.html")
 if __name__ == '__main__':
    app.run()

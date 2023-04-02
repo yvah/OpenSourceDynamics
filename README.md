4571848845d74ffb999b42d89ebb4be1d40a4ed2
Healyr7 <healyr7@tcd.ie>
2023-04-02 02:46:05 +0100

    README updated, some TODOs and comments added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SwEng-group13
 To make it run make sure: 
-run -- npm install chart.js --save 
+you enter the python venv
+run (in termial in vscode (works on linux)):
+. venv/bin/activate
+from the project directory
--- templates/script.js
+++ templates/script.js
@@ -1,12 +1,46 @@
+//Create a Cognos Dashboard Embedded Session
+var createSession = function(){
 
-      function go()
-      {
-         let pToken = document.getElementById('password')
-         pToken = pToken.value
-         let repo = document.getElementById('name')
-         repo = repo.value
-         let pullType = document.getElementById('pulltype')
-         console.log(repo);
-         console.log(pToken);
-         console.log(pullType);
+   const createSessionUrl = "/session";
+
+   var clientId = document.getElementById('client-id-input').value;
+   var clientSecret = document.getElementById('client-secret-input').value;
+   var webDomain = window.location.href;
+
+   fetch(createSessionUrl, {
+       method : "POST",
+       body: JSON.stringify({clientId: clientId, clientSecret: clientSecret, webDomain: webDomain})
+   }).then(
+       response => response.json() // .json(), etc.
+       // same as function(response) {return response.text();}
+   ).then(
+      function(resp){
+         console.log(resp);
+
+      //   var sessionCodeSpan = document.getElementById("session-code-span");
+      //   sessionCodeSpan.innerHTML = resp.sessionCode;
+      //   sessionCodeSpan.dataset.sessionCode = resp.sessionCode;
+
+      //   //Persist client ID for use later
+      //   var clientIdDataSpan = document.getElementById("client-id-data-span");
+      //   clientIdDataSpan.dataset.clientId = clientId;
+      
+      // loadDashboards(clientId);
+
+      var api = new CognosApi({cognosRootURL: 'https://eu-gb.dynamic-dashboard-embedded.cloud.ibm.com/daas/',
+         node: document.getElementById('cognos'),
+         sessionCode: resp.sessionCode,
+         language: 'en'
+      })
+      api.initialize()
+      const dashboardApi = null;
+      api.dashboard.openDashboard({
+         dashboardSpec: {...} // preset dashboard, with preloaded data
+      }).then((dashboardAPI) => {
+         console.log('Dashboard opened successfully.');
+         dashboardApi = dashboardAPI;
+      });
+
       }
+   );
+};

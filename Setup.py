dcf41791aab82f1e12a7c9ec6140c3998fcb5b26
taylorj8 <taylorj8@tcd.ie>
2023-04-02 23:02:06 +0100

    webpage brings you to dashboard

--- Setup.py
+++ Setup.py
@@ -5,6 +5,7 @@ import json
 import webbrowser
 app = Flask(__name__, static_folder="templates, fetched_data", static_url_path="")
 
+
 @app.route("/", methods=["GET"])
 def index():
     with open(r"data/tables.txt", 'r') as fp:
@@ -15,6 +16,7 @@ def index():
              
     print(pre_pulled)
     return render_template("index.html", pre_pulled=pre_pulled)
+
 
 @app.route("/query", methods=["POST"])
 def query():
@@ -27,12 +29,14 @@ def query():
     use_new_data(user,repo,type)
     return render_template("index.html")
 
+
 @app.route("/pulled", methods=["POST"])
 def pulled():
     repo = request.form["Pre-Pulled Data"]
     use_existing_data(repo)
     
-    return webbrowser.open("https://eu-gb.dataplatform.cloud.ibm.com/dashboards/76dcd7cf-ca85-4654-b47a-02610eac5955/view/5f65e53600b009ea6de3c4e40799280374312255b0bb8a01d6837b4909642297a86a4093c87c1e08d9175363f5b91a5acd")
+    return render_template("dashboard.html")
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
 # use https://raw.githubusercontent.com/yvah/SwEng-group13/API/query.py as source for query

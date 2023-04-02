1b806f7b2442139524fa1cec16c0a32420b38e10
jcb4321 <bryansj@tcd.ie>
2023-04-02 20:07:21 +0100

    commiting selector

--- Setup.py
+++ Setup.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 from bridge import run_all
 from pr_nlu_analysis import Repository
 from time import time
+import numpy
 import json
-app = Flask(__name__, static_folder="templates", static_url_path="")
+import webbrowser
+app = Flask(__name__, static_folder="templates, fetched_data", static_url_path="")
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    pre_pulled = numpy.array(["Flutter", "swEng", "Microsoft Windows"])
+    return render_template("index.html", pre_pulled=pre_pulled)
 
 @app.route("/query", methods=["POST"])
 def query():
@@ -19,6 +22,13 @@ def query():
     print(user,repo,type)
     run_all(user,repo,type)
     return render_template("index.html")
+
+@app.route("/pulled", methods=["POST"])
+def pulled():
+    repo = request.form["Pre-Pulled Data"]
+    print(repo)
+    
+    return webbrowser.open("https://eu-gb.dataplatform.cloud.ibm.com/dashboards/76dcd7cf-ca85-4654-b47a-02610eac5955/view/5f65e53600b009ea6de3c4e40799280374312255b0bb8a01d6837b4909642297a86a4093c87c1e08d9175363f5b91a5acd")
 if __name__ == '__main__':
    app.run()
 # use https://raw.githubusercontent.com/yvah/SwEng-group13/API/query.py as source for query

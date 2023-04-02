ff10dccdfdfc849e5d34ab241dfc76edcbeb856b
taylorj8 <taylorj8@tcd.ie>
2023-04-02 21:50:36 +0100

    prepared for additional data

--- bridge.py
+++ bridge.py
@@ -8,32 +8,33 @@ import json
 # switch source to existing data set
 def use_existing_data(table):
     db = DB2("credentials/db2_credentials.json")
-    db.copy_into("SOURCE", table)
+    db.switch_sources(table)
     db.close()
 
 
 # gather from new repo and switch source
 def use_new_data(auth, repo, pull_type):
     # database = cloudant.CDatabase("credentials/cloudant_credentials.json")
-    db = DB2("credentials/db2_credentials.json")
 
     owner_repo = repo.split("/")
     table = f"{owner_repo[0]}_{owner_repo[1]}_{pull_type}"
     add_name(table)
 
-    # create and clear the table in case it doesn't exist or already has data
-    db.create(table)
-    db.clear(table)
-
-    # collect data and add it to the database
+    # collect and analyse data
     data = run_query(auth, owner_repo[0], owner_repo[1], pull_type)
     repo = Repository(data, pull_type)
-    db.add_data(table, repo.repo_items)
+
+    # create connection to DB2
+    db = DB2("credentials/db2_credentials.json")
+    # create and clear the table in case it doesn't exist or already has data
+    db.create_all(table)
+    db.clear_all(table)
+    db.add_data(table, repo.repo_items)  # todo add data to other table
     # repo.to_csv()
     # repo.stats_to_csv()
 
-    # switch the source to the gathered data
-    db.copy_into("SOURCE", table)
+    # switch the sources to the gathered data
+    db.switch_sources(table)
     db.close()
 
 
--- db.py
+++ db.py
@@ -7,6 +7,44 @@ def load_json(filename):
         return json.load(json_file)
 
 
+# returns string containing question marks for sql statement
+def get_question_marks(number):
+    string = "?"
+    for _ in range(number-1):
+        string += ",?"
+
+
+# function returns the parameters for the execute_many command
+def populate_params(data, table_type):
+    if data is None:
+        return []
+
+    # parameters vary based on type of table
+    params = []
+    if table_type == "source":
+        for d in data:
+            params.append((d.number, d.title, d.author, d.gender, d.state, d.createdAt, d.closedAt, d.lifetime,
+                           d.number_of_comments, d.sentiment, d.emotion[0][1], d.emotion[1][1], d.emotion[2][1],
+                           d.emotion[3][1], d.emotion[4][1]))
+    elif table_type == "average":
+        for d in data:
+            params.append((d.sentiment, d.emotion[0][1], d.emotion[1][1], d.emotion[2][1], d.emotion[3][1],
+                           d.emotion[4][1], d.lifetime))
+    elif table_type == "state":
+        for d in data:
+            params.append((d.state, d.frequency, d.sentiment, d.emotion[0][1], d.emotion[1][1], d.emotion[2][1],
+                           d.emotion[3][1], d.emotion[4][1], d.lifetime))
+    elif table_type == "gender":
+        for d in data:
+            params.append((d.gender, d.frequency, d.sentiment, d.emotion[0][1], d.emotion[1][1], d.emotion[2][1],
+                           d.emotion[3][1], d.emotion[4][1], d.lifetime))
+    return tuple(params)
+
+
+sql_dict = load_json("sql.json")
+num_dict = {"source": 15, "average": 7, "state": 9, "gender": 9}
+
+
 class DB2:
     def __init__(self, credentials_file):
         self.credentials = load_json(credentials_file)
@@ -24,24 +62,8 @@ class DB2:
         print("connection successful")
 
     # creates a table with with the passed in name, returns true on success or if table already exists
-    def create(self, table):
-        sql_instruction = f"CREATE TABLE {self.schema + table} (" \
-                          "Number int NOT NULL, " \
-                          "Title varchar(255), " \
-                          "Author varchar(255), " \
-                          "Gender varchar(255), " \
-                          "State varchar(255), " \
-                          "Created varchar(255), " \
-                          "Closed varchar(255), " \
-                          "Lifetime float, " \
-                          "Comments int, " \
-                          "Sentiment float, " \
-                          "Sadness float, " \
-                          "Joy float, " \
-                          "Fear float, " \
-                          "Disgust float, " \
-                          "Anger float, " \
-                          "PRIMARY KEY (Number));"
+    def create(self, table, table_type):
+        sql_instruction = f"CREATE TABLE {self.schema + table} (" + sql_dict[table_type]
 
         try:
             ibm_db.exec_immediate(self.connection, sql_instruction)
@@ -59,23 +81,37 @@ class DB2:
             print("error clearing table")
             return False
 
+    # creates all the different types of tables
+    def create_all(self, table):
+        self.create(table, "source")
+        self.create(table + "_average", "average")
+        self.create(table + "_state", "state")
+        self.create(table + "_gender", "gender")
+
+    # clears all the different types of tables
+    def clear_all(self, table):
+        if table != "sources":
+            self.clear(table)
+            self.clear(table + "_average")
+            self.clear(table + "_state")
+            self.clear(table + "_gender")
+        else:
+            self.clear("SOURCE")
+            self.clear("AVERAGE")
+            self.clear("STATE")
+            self.clear("GENDER")
+
     # adds the data to a table, creating the table if it doesn't already exist
     # returns true if successful, otherwise prints number of successful inserts and returns false
-    def add_data(self, table, data):
+    def add_data(self, table, data, table_type):
         # creates table if it doesn't already exist
-        self.create(table)
-        insert_command = f"INSERT INTO {self.schema + table} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+        self.create(table, table_type)
+        insert_command = f"INSERT INTO {self.schema + table} VALUES ({get_question_marks(num_dict[table_type])})"
         insert_command = ibm_db.prepare(self.connection, insert_command)
 
         # adds this data to the table
-        params = []
-        if data is not None:
-            for d in data:
-                params.append((d.number, d.title, d.author, d.gender, d.state, d.createdAt, d.closedAt, d.lifetime,
-                               d.number_of_comments, d.sentiment, d.emotion[0][1], d.emotion[1][1], d.emotion[2][1],
-                               d.emotion[3][1], d.emotion[4][1]))
-
-        success = ibm_db.execute_many(insert_command, tuple(params))
+        params = populate_params(data, table_type)
+        success = ibm_db.execute_many(insert_command, params)
         if success is not False:
             return True
         row_count = ibm_db.num_rows(insert_command)
@@ -83,14 +119,19 @@ class DB2:
         return False
 
     # copies the data from one table into another
-    def copy_into(self, to_table, from_table):
-        success = self.clear(to_table)
-        if success:
-            sql_instruction = f"INSERT INTO {self.schema + to_table} SELECT * FROM {self.schema + from_table};"
-            success = ibm_db.exec_immediate(self.connection, sql_instruction)
-            if success is not False:
-                return True
-        return False
+    def switch_sources(self, table):
+        self.clear_all("sources")
+
+        sql_statement = f"INSERT INTO ? SELECT * FROM ?;"
+        sql_statement = ibm_db.prepare(self.connection, sql_statement)
+        full_table_name = self.schema + table
+
+        # populate parameters and run all
+        params = ((f"{self.schema}SOURCE", full_table_name),
+                  (f"{self.schema}AVERAGE", f"{full_table_name}_average"),
+                  (f"{self.schema}STATE", f"{full_table_name}_state"),
+                  (f"{self.schema}GENDER", f"{full_table_name}_gender"))
+        ibm_db.execute_many(sql_statement, params)
 
     # close the connection
     def close(self):

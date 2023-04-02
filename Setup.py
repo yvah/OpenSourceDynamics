d8b99e05485c91966f78dac089e8e39ef384282c
jcb4321 <bryansj@tcd.ie>
2023-04-02 21:04:46 +0100

    commiting select

--- Setup.py
+++ Setup.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request
-from bridge import run_all
-from pr_nlu_analysis import Repository
+from bridge import use_existing_data, use_new_data
 from time import time
-import numpy
 import json
 import webbrowser
 app = Flask(__name__, static_folder="templates, fetched_data", static_url_path="")
 
 @app.route("/", methods=["GET"])
 def index():
-    with open(r'tables.txt', 'r') as fp:
+    with open(r"data/tables.txt", 'r') as fp:
         lines = fp.readlines()
+        pre_pulled = []
         for row in lines:
-            pre_pulled = numpy.add([row])
+             pre_pulled.append(row)
+             
+    print(pre_pulled)
     return render_template("index.html", pre_pulled=pre_pulled)
 
 @app.route("/query", methods=["POST"])
@@ -23,13 +24,13 @@ def query():
     type = request.form["type"]
     #owner_repo = repo.split("/")
     print(user,repo,type)
-    run_all(user,repo,type)
+    use_new_data(user,repo,type)
     return render_template("index.html")
 
 @app.route("/pulled", methods=["POST"])
 def pulled():
     repo = request.form["Pre-Pulled Data"]
-    print(repo)
+    use_existing_data(repo)
     
     return webbrowser.open("https://eu-gb.dataplatform.cloud.ibm.com/dashboards/76dcd7cf-ca85-4654-b47a-02610eac5955/view/5f65e53600b009ea6de3c4e40799280374312255b0bb8a01d6837b4909642297a86a4093c87c1e08d9175363f5b91a5acd")
 if __name__ == '__main__':
